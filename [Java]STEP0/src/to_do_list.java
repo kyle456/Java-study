6a5ec731189e1b1664f59b47a72fff7f655df882
yjookim <kyle2123@naver.com>
2020-08-20 02:27:50 +0900

    [Java]STEP0 - to do list 일정 검색 기능
    <변수 설명>
    findDate: 사용자가 찾고자 입력한 날짜
    schedule: 사용자가 찾고자 하는 날짜에 해당 하는 일정 arraylist
    
    <동작 구조>
    1. 먼저 사용자가 찾고자 하는 날짜를 입력 받는다.
    2. Line 47의 조건문을 통해 해당 날짜에 일정이 있으면 정상진행, 일정이 없으면 안내문과 함께 break.
    3. schedule Arraylist를 통해서 해당 날짜에 해당하는 일정 리스트를 가져온다.
    4. schedule.size() 를 이용해서 일정의 개수를 알려준다.
    5. Line 52의 반복문을 통해 schedule.size(), 즉 일정의 개수 만큼 일정의 내용을 표시한다.

--- [Java]STEP0/src/to_do_list.java
+++ [Java]STEP0/src/to_do_list.java
@@ -17,55 +17,65 @@ public class to_do_list {
 		System.out.println("+----------------------+");
 
 		while (true) {
-			System.out.print("명령 (1, 2, 3, h, q)\n> ");
+			System.out.print("명령 (1.일정 등록  2.일정 검색 및 변경  3.달력 보기  h.도움말  q.종료)\n> ");
 			char order = scanner.next().charAt(0); // choose menu
 			scanner.nextLine();
 
 			switch (order) {
 			case '1':
-				System.out.print("[일정 등록] 날짜를 입력하세요.\n> ");
+				System.out.print("[일정 등록] 날짜를 입력하세요.(ex.2020-01-01)\n> ");
 				String theDate = scanner.nextLine();
-				
+
 				if (!listMap.containsKey(theDate)) {
 					ArrayList<String> emptyList = new ArrayList<String>();
 					listMap.put(theDate, emptyList);
 				}
-				
+
 				System.out.print("[일정 등록] 일정을 입력하세요.\n> ");
 				String theList = scanner.nextLine();
-				
+
 				ArrayList<String> existList = listMap.get(theDate);
 
 				existList.add(theList);
 				listMap.put(theDate, existList);
 				break;
-				
+
 			case '2':
-				System.out.print("찾기 > ");
-				String c = scanner.nextLine();
+				System.out.print("[일정 검색] 날짜를 입력하세요.(ex.2020-01-01)\n> ");
+				String findDate = scanner.nextLine();
 
-				System.out.println(listMap.get(c));
+				if (listMap.containsKey(findDate)) {
+					ArrayList<String> schedule = listMap.get(findDate);
 
+					System.out.printf("%d개의 일정이 있습니다.\n", schedule.size());
+
+					for (int i = 0; i < schedule.size(); i++) {
+						System.out.printf("%d.%s\n", i + 1, schedule.get(i));
+					}
+				} else {
+					System.out.println("해당 날짜에 일정이 존재하지 않습니다.");
+				}
 				break;
-				
+
 			case '3':
 
 				break;
-				
+
 			case 'h':
+				System.out.println("");
 				System.out.println("<일정 관리 프로그램 도움말>");
 				System.out.println("(1. 일정 등록)을 선택하시면 일정을 등록하고 싶은 날짜와 일정 내용을 입력하시면 해당 일정이 저장됩니다.");
 				System.out.println("(2. 일정 검색 및 변경)을 선택하시면 검색하고 싶은 날짜를 입력하시면 해당 날짜에 저장 된 일정을 보여줍니다. 일정 변경 또한 가능합니다.");
-				System.out.println("(3. 달력 보기)를 선택하시면 오늘 날짜를 자동으로 인식하여 해당 월의 달력을 보여줍니다.");
+				System.out.println("(3. 달력 보기)를 선택하시면 원하시는 년도와 월의 달력을 보여줍니다.");
 				System.out.println("(h. 도움말)을 선택하시면 지금 보고 계시는 이 도움말을 보여줍니다.");
 				System.out.println("(q. 종료)를 선택하시면 일정 관리 프로그램이 종료됩니다.");
 				System.out.println("");
 				break;
-				
+
 			case 'q':
 				System.out.println("종료합니다. 이용해 주셔서 감사합니다.");
 				System.exit(0);
-				
+
 			default:
 				System.out.println("잘 못 입력하셨습니다. 다시 선택해주세요.");
 			}
