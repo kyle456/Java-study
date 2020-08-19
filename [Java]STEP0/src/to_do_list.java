3068d457f1c55fbb9f7232074acb4ef278633d23
yjookim <kyle2123@naver.com>
2020-08-20 01:35:53 +0900

    [Java]STEP0 - to do list 일정 등록 기능
    <변수 설명>
    listMap: key값은 날짜, value 값은 일정 리스트 를 가지는 HashMap
    theDate: 사용자에게서 입력받는 날짜
    emptyList: 만약 해당 날짜에 일정이 없다면, listMap에 해당 날짜를 key로, emptyList를 value로 담기위한 arraylist
    theList: 사용자에게서 입력받는 일정
    existList: 해당 날짜에 이미 일정이 있다면, theDate를 key로 하는 value인 arraylist를 불러오기 위한 변수
    
    <동작 구조>
    1. HashMap 안에 key는 String으로, value는 Arraylist 로 지정하였다.
    2. 여기서 key는 사용자가 입력하는 날짜이고, value는 그 날짜에 입력하는 일정들이다. 일정이 한 개 이상이므로 Arraylist로 담았다.
    3. Line 29의 조건문은 사용자가 입력하는 날짜에 일정이 이미 있는지 없는지를 판별한다.
    4. 일정이 없다면 해당 key에 대한 arraylist가 만들어지지 않았기 때문에 에러가 난다. 따라서 emptyList를 통해 빈 arraylist를 만들어 준다.
    5. 일정이 이미 들어있다면 단순히 put을 통해서 이미 들어있는 일정 위에 새로운 일정을 추가한다.

--- [Java]STEP0/src/to_do_list.java
+++ [Java]STEP0/src/to_do_list.java
@@ -1,9 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class to_do_list {
 
 	public static void main(String[] args) {
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
+		HashMap<String, ArrayList<String>> listMap = new HashMap<String, ArrayList<String>>();
 
 		System.out.println("+----------------------+");
 		System.out.println("| 1. 일정 등록");
@@ -13,33 +17,57 @@ public class to_do_list {
 		System.out.println("+----------------------+");
 
 		while (true) {
-			System.out.print("명령 (1, 2, 3, h, q)\n>");
-			char order = scanner.next().charAt(0);
-			
-			switch(order) {
-	        case '1': 
-	            
-	            break;
-	        case '2':
-	            
-	            break;
-	        case '3':
-	            
-	            break;
-	        case 'h':
-	            System.out.println("<일정 관리 프로그램 도움말>");
-	            System.out.println("(1. 일정 등록)을 선택하시면 일정을 등록하고 싶은 날짜와 일정 내용을 입력하시면 해당 일정이 저장됩니다.");
-	            System.out.println("(2. 일정 검색 및 변경)을 선택하시면 검색하고 싶은 날짜를 입력하시면 해당 날짜에 저장 된 일정을 보여줍니다. 일정 변경 또한 가능합니다.");
-	            System.out.println("(3. 달력 보기)를 선택하시면 오늘 날짜를 자동으로 인식하여 해당 월의 달력을 보여줍니다.");
-	            System.out.println("(h. 도움말)을 선택하시면 지금 보고 계시는 이 도움말을 보여줍니다.");
-	            System.out.println("(q. 종료)를 선택하시면 일정 관리 프로그램이 종료됩니다.");
-	            System.out.println("");
-	            break;
-	        case 'q':
-	        	System.out.println("종료합니다. 이용해 주셔서 감사합니다.");
-	            System.exit(0);
-	        default :
-	            System.out.println("잘 못 입력하셨습니다. 다시 선택해주세요.");
+			System.out.print("명령 (1, 2, 3, h, q)\n> ");
+			char order = scanner.next().charAt(0); // choose menu
+			scanner.nextLine();
+
+			switch (order) {
+			case '1':
+				System.out.print("[일정 등록] 날짜를 입력하세요.\n> ");
+				String theDate = scanner.nextLine();
+				
+				if (!listMap.containsKey(theDate)) {
+					ArrayList<String> emptyList = new ArrayList<String>();
+					listMap.put(theDate, emptyList);
+				}
+				
+				System.out.print("[일정 등록] 일정을 입력하세요.\n> ");
+				String theList = scanner.nextLine();
+				
+				ArrayList<String> existList = listMap.get(theDate);
+
+				existList.add(theList);
+				listMap.put(theDate, existList);
+				break;
+				
+			case '2':
+				System.out.print("찾기 > ");
+				String c = scanner.nextLine();
+
+				System.out.println(listMap.get(c));
+
+				break;
+				
+			case '3':
+
+				break;
+				
+			case 'h':
+				System.out.println("<일정 관리 프로그램 도움말>");
+				System.out.println("(1. 일정 등록)을 선택하시면 일정을 등록하고 싶은 날짜와 일정 내용을 입력하시면 해당 일정이 저장됩니다.");
+				System.out.println("(2. 일정 검색 및 변경)을 선택하시면 검색하고 싶은 날짜를 입력하시면 해당 날짜에 저장 된 일정을 보여줍니다. 일정 변경 또한 가능합니다.");
+				System.out.println("(3. 달력 보기)를 선택하시면 오늘 날짜를 자동으로 인식하여 해당 월의 달력을 보여줍니다.");
+				System.out.println("(h. 도움말)을 선택하시면 지금 보고 계시는 이 도움말을 보여줍니다.");
+				System.out.println("(q. 종료)를 선택하시면 일정 관리 프로그램이 종료됩니다.");
+				System.out.println("");
+				break;
+				
+			case 'q':
+				System.out.println("종료합니다. 이용해 주셔서 감사합니다.");
+				System.exit(0);
+				
+			default:
+				System.out.println("잘 못 입력하셨습니다. 다시 선택해주세요.");
 			}
 		}
 	}
