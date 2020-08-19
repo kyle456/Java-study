f8c6395603d66b04c0a4c8a03f715243fbb6c07a
yjookim <kyle2123@naver.com>
2020-08-19 21:00:36 +0900

    [Java]STEP0 - to do list의 뼈대 만들기
    <만드려고하는 to do list의 요구사항>
    간단한 콘솔 기반 사용자 UI를 만든다. (일정 검색용)
    오늘 날짜를 인식해서 해당 월의 달력을 출력한다.
    달력에서 일정이 있는 날 아래에는 . 을 찍어서 보여준다.
    일정을 등록, 검색, 변경이 가능하게 해 준다.
    오늘의 일정을 표시해 준다.
    
    <현재까지 해결>
    1. 사용자 UI 제작
    2. switch case 문으로 사용자의 입력에 따른 기능 분화
    3. default는 보기에 있는 명령을 제외한 나머지 명령이 들어왔을 때 안내문과 함께 while을 다시 반복하도록 설정

---
+++ [Java]STEP0/src/to_do_list.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class to_do_list {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("+----------------------+");
+		System.out.println("| 1. 일정 등록");
+		System.out.println("| 2. 일정 검색 ");
+		System.out.println("| 3. 달력 보기");
+		System.out.println("| h. 도움말    q. 종료");
+		System.out.println("+----------------------+");
+
+		while (true) {
+			System.out.print("명령 (1, 2, 3, h, q)\n>");
+			char order = scanner.next().charAt(0);
+			
+			switch(order) {
+	        case '1': 
+	            
+	            break;
+	        case '2':
+	            
+	            break;
+	        case '3':
+	            
+	            break;
+	        case 'h':
+	            
+	            break;
+	        case 'q':
+	            
+	            break;
+	        default :
+	            System.out.println("잘 못 입력하셨습니다. 다시 선택해주세요.");
+			}
+		}
+	}
+
+}
