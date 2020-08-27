eda291df8c4429ee40eb434e85c6e113a5a807f8
yjookim <kyle2123@naver.com>
2020-08-28 02:24:10 +0900

    [Java]Algorithm - 2020.08.28 10039번 평균점수

---
+++ [Java]Algorithm/src/BaekJoon/B200828_Average_Score.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class B200828_Average_Score {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int total = 0;
+
+		for (int i = 0; i < 5; i++) {
+			int score = scanner.nextInt();
+
+			if (score < 40) {
+				score = 40;
+			}
+
+			total += score;
+		}
+		
+		System.out.printf("%d", total / 5);
+
+	}
+
+}
