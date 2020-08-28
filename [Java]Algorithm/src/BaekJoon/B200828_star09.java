229f037f36d26da6391c6aa3d9e9547767575398
yjookim <kyle2123@naver.com>
2020-08-28 17:10:25 +0900

    [Java]Algorithm - 2446번: 별 찍기-09 풀이와 마크다운 파일

---
+++ [Java]Algorithm/src/BaekJoon/B200828_star09.java
@@ -0,0 +1,47 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B200828_star09 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int second = num * 2 - 1;
+
+		for (int i = 0; i < num; i++) {
+			for (int j = 0; j < second; j++) {
+				if (j < i) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			System.out.println(" ");
+			second--;
+		}
+
+		second += 2;
+
+		for (int i = num - 2; i >= 0; i--) {
+			for (int j = 0; j < second; j++) {
+				if (j < i) {
+					System.out.print(" ");
+				} else {
+					System.out.print("*");
+				}
+			}
+			if (i == 0) {
+				System.out.println();
+			} else {
+				System.out.println(" ");
+			}
+			second++;
+		}
+
+		br.close();
+	}
+
+}
