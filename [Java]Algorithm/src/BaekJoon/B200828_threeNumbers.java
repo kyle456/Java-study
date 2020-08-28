40dee3f99fdbf4567c16d0a913df6bf7e9e96c92
yjookim <kyle2123@naver.com>
2020-08-28 15:42:57 +0900

    [Java]Algorithm - 10817번: 세 수 풀이와 마크다운 파일

---
+++ [Java]Algorithm/src/BaekJoon/B200828_threeNumbers.java
@@ -0,0 +1,25 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class B200828_threeNumbers {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String a = br.readLine();
+		String[] b = a.split(" ");
+		int[] number = new int[3];
+
+		for (int i = 0; i < 3; i++) {
+			number[i] = Integer.parseInt(b[i]);
+		}
+
+		Arrays.sort(number);
+		System.out.print(number[1]);
+
+	}
+
+}
