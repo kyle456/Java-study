e70e6c216a2db3e8a052a243d84a0d9e6f9b8850
yjookim <kyle2123@naver.com>
2020-08-28 16:18:29 +0900

    [Java]Algorithm - 2523번: 별 찍기-13 풀이와 마크다운 파일 생성

---
+++ [Java]Algorithm/src/BaekJoon/B200828_star13.java
@@ -0,0 +1,45 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B200828_star13 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+		int i = 1, j = 1;
+		
+		while (i <= num) {			
+			System.out.print("*");
+			
+			if(j < i) {
+				j++;
+				continue;
+			}
+			
+			i++;
+			j = 1;
+			System.out.println();
+		}
+		
+		i -= 2;
+		
+		while (i > 0) {			
+			System.out.print("*");
+			
+			if(j < i) {
+				j++;
+				continue;
+			}
+			
+			i--;
+			j = 1;
+			System.out.println();
+		}
+		
+		br.close();		
+	}
+
+}
