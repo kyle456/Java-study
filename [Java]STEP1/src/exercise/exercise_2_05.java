f43e2d1425c903d23ef24bbe234784a0bca3fbde
yjookim <kyle2123@naver.com>
2020-08-29 18:07:07 +0900

    [Java]STEP1 - exercise 2-5 상수 -> 변수로 변경

--- [Java]STEP1/src/exercise/exercise_2_05.java
+++ [Java]STEP1/src/exercise/exercise_2_05.java
@@ -7,7 +7,7 @@ public class exercise_2_05 {
 
 	public static void main(String[] args) {
 		int value = 356;
-		System.out.println(356 / 100 * 100);
+		System.out.println((value / 100) * 100);
 
 	}
 
