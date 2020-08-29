432f42791f87217fd5162ba3338e9b3c099f5e02
yjookim <kyle2123@naver.com>
2020-08-29 18:04:53 +0900

    [Java]STEP1 - exercise 2-5 십의 자리 이하 버리기
    산술 연산자 공부

---
+++ [Java]STEP1/src/exercise/exercise_2_05.java
@@ -0,0 +1,14 @@
+// 십의 자리 이하를 버리는 코드.
+// value가 356일 때 300이 나올 수 있도록 하여라.
+
+package exercise;
+
+public class exercise_2_05 {
+
+	public static void main(String[] args) {
+		int value = 356;
+		System.out.println(356 / 100 * 100);
+
+	}
+
+}
