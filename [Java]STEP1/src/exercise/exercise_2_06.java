e6e6590ae974ffa069c5431baef6f200ee845e11
yjookim <kyle2123@naver.com>
2020-08-29 18:08:27 +0900

    [Java]STEP1 - exercise 2-6 사다리꼴의 넓이 구하기
    산술 연산자 공부

---
+++ [Java]STEP1/src/exercise/exercise_2_06.java
@@ -0,0 +1,15 @@
+// 윗면 5, 아랫면 10, 높이 7인 사다리꼴의 넓이 구하기
+
+package exercise;
+
+public class exercise_2_06 {
+
+	public static void main(String[] args) {
+		int lengthTop = 5;
+		int lengthBottom = 10;
+		int height = 7;
+		double area = ((lengthTop + lengthBottom) * height) / 2.0;
+		System.out.println(area);
+	}
+
+}
