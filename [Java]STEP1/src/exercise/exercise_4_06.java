2208a30eba90a5211629b4f03bb8c4fc4037e3c7
yjookim <kyle2123@naver.com>
2020-09-04 19:44:43 +0900

    [Java]STEP1 - exercise 4-06 별 삼각형 출력
    for문을 이용하여 별로 직각삼각형 모양 출력하기
    이중 for문을 사용하지 않음
    대신 String에 별을 하나씩 더하면서 늘어나게 하여 for문 하나 만으로도 구현하였음

---
+++ [Java]STEP1/src/exercise/exercise_4_06.java
@@ -0,0 +1,17 @@
+// for문을 이용하여 *로 직각삼각형 출력하기
+
+package exercise;
+
+public class exercise_4_06 {
+
+	public static void main(String[] args) {
+		String star = "";
+
+		for (int i = 0; i < 5; ++i) {
+			star += "*";
+			System.out.println(star);
+		}
+
+	}
+
+}
