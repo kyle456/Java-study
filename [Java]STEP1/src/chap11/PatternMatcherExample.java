c845c796015f57927f4c8dc8eab0bc8615d45551
kyle456 <kyle2123@naver.com>
2020-11-23 02:52:13 +0900

    [Java]STEP1 - chap11 exercise 10 정규표현식 수정

--- [Java]STEP1/src/chap11/PatternMatcherExample.java
+++ [Java]STEP1/src/chap11/PatternMatcherExample.java
@@ -6,7 +6,7 @@ public class PatternMatcherExample {
 
 	public static void main(String[] args) {
 		String id = "5Angel1004";
-		String regExp = "[a~zA~Z]\\w+";
+		String regExp = "[a~zA~Z][a~zA~Z0~9]{7,11}";
 		boolean isMatch = Pattern.matches(regExp, id);
 		if (isMatch) {
 			System.out.println("ID로 사용할 수 있습니다.");
