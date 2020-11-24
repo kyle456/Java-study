917bafa453f2607894ddf8b5412494f796188afa
kyle456 <kyle2123@naver.com>
2020-11-24 14:23:11 +0900

    [Java]STEP2 - chap13 exercise 4 equals()를 ==로 변경

--- [Java]STEP1/src/chap13/Util.java
+++ [Java]STEP1/src/chap13/Util.java
@@ -2,7 +2,7 @@ package chap13;
 
 public class Util {
 	public static <T, S> S getValue(Pair<T, S> pair, T key) {
-		if (key.equals(pair.getKey())) {
+		if (key == pair.getKey()){
 			return pair.getData();
 		}
 		return null;
