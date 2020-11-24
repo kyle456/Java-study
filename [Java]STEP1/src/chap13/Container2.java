649d16d6a6f95d944e132e714e713b0a16b44d3a
kyle456 <kyle2123@naver.com>
2020-11-24 13:08:28 +0900

    [Java]STEP2 - chap13 exercise 3 풀이

---
+++ [Java]STEP1/src/chap13/Container2.java
@@ -0,0 +1,19 @@
+package chap13;
+
+public class Container2<T, S> {
+	private T key;
+	private S value;
+	
+	public T getKey() {
+		return key;
+	}
+	
+	public S getValue() {
+		return value;
+	}
+	
+	public void set(T key, S value) {
+		this.key = key;
+		this.value = value;
+	}
+}
